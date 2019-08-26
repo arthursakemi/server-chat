30786cae9d969ed06dd8c6ad3a19818e9c8ec3b1
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-08-26 16:32:08 -0300

    Socket working.

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const config = require('./config/config')
+const http = require('http')
 const port = process.argv[2] || 3000
-const app = config.setUpServer()
+const App = config.setUpServer()
+const server = http.createServer(App)
+var io = require('socket.io')(server);
+    io.on('connection', (socket) => {
+        console.log('User connected')
+        socket.on('chat message', (msg) => {
+            io.emit('chat message',  msg)
+        })
+        socket.on('disconnect', function(){
+            console.log('user disconnected')
+        })
+    })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('-----------------------\n|      SERVER ON!     |\n|     Porta: ' + port + '     |\n-----------------------')
 })
--- config/config.js
+++ config/config.js
@@ -1,17 +1,18 @@
     const express = require('express')
     require('../model/mongoose')
-
+    
     module.exports.setUpServer = () => {
         const app = express()
+        
         const bodyParser = require('body-parser')
         const path = require('path')
+        
+
         app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         });
-
-        // app.use('/', express.static(path.join('../', '/public', 'html')))
 
         app.use(bodyParser.json())
         app.use(bodyParser.urlencoded({ extended: true }))
